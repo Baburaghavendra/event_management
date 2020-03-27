1939cd912af19b223d024c496987621c0bdf7da5
Baburaghavendra <38286601+Baburaghavendra@users.noreply.github.com>
2020-03-27 18:36:59 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,9 +7,3 @@ submit.addEventListener('click', e => {
   form.reset();
 }, false);
 
-
-// ----News&Events-----
-$(document).ready(function() {
-    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
-    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
-});
